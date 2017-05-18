549fcc17d0d061004a711e9ea4848470c57e09a7
camel113 <adrienbigler@gmail.com>
2017-05-18 10:27:37 +0200

    Start to implement TV layout

--- src/App.js
+++ src/App.js
@@ -3,61 +3,71 @@ import logo from './logo.svg';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
   IndexRoute,
   browserHistory,
   Link
 } from 'react-router-dom'
 import Home from './Home.js'
+import Tv from './Tv.js'
 import './App.css';
 
-const Admin = () => (
+const DefaultLayout = ({ children }) => (                       
   <div>
-    <h2>Admin</h2>
-  </div>
-)
+    <header className="primary-header">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-4 hidden-sm-down"></div>
+          <div className="col-6 col-md-4 teampulse-logo"><img src="teampulse_160px.png" alt="Teampulse"/></div>
+          <div className="col-6 col-md-4 race-logo"><img src="race_160px.png"  alt="Race Across America 17"/></div>
+        </div>
+      </div>
+    </header>
+    <aside className="primary-aside"></aside>
+    {children}
+    <div className="footer">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="d-flex col-4 col-lg-1 text-center logo-sponsor">
+            <img className="img-fluid" src="sponsors/vaudh.png" />
+          </div>
+          <div className="d-flex col-4 col-lg-1 text-center logo-sponsor">
+            <img className="img-fluid" src="sponsors/hesav.png" />
+          </div>
+          <div className="d-flex col-4 col-lg-1 text-center logo-sponsor">
+            <img className="img-fluid" src="sponsors/heig.png" />
+          </div>
+          <div className="d-flex col-6 col-lg-1 text-center logo-sponsor">
+           <img className="img-fluid" src="sponsors/chuv.png" />
+          </div>
+          <div className="d-flex col-6 col-lg-1 text-center logo-sponsor">
+            <img className="img-fluid" src="sponsors/unil.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="visual-footer hidden-lg-up">
+      <div className="container-fluid">
+        
+      </div>
+    </div>                                      
+  </div>           
+); 
+const TvLayout = ({ children }) => (                       
+  <div>
+    {children}                              
+  </div>           
+); 
 
 
 const BasicExample = () => (
   <Router>
     <div>
-      <header className="primary-header">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-4 hidden-sm-down"></div>
-            <div className="col-6 col-md-4 teampulse-logo"><img src="teampulse_160px.png" alt="Teampulse"/></div>
-            <div className="col-6 col-md-4 race-logo"><img src="race_160px.png"  alt="Race Across America 17"/></div>
-          </div>
-        </div>
-      </header>
-      <aside className="primary-aside"></aside>
-
-      <Route exact path="/" component={Home}/>
-      <div className="footer">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="d-flex col-4 col-lg-1 text-center logo-sponsor">
-              <img className="img-fluid" src="sponsors/vaudh.png" />
-            </div>
-            <div className="d-flex col-4 col-lg-1 text-center logo-sponsor">
-              <img className="img-fluid" src="sponsors/hesav.png" />
-            </div>
-            <div className="d-flex col-4 col-lg-1 text-center logo-sponsor">
-              <img className="img-fluid" src="sponsors/heig.png" />
-            </div>
-            <div className="d-flex col-6 col-lg-1 text-center logo-sponsor">
-             <img className="img-fluid" src="sponsors/chuv.png" />
-            </div>
-            <div className="d-flex col-6 col-lg-1 text-center logo-sponsor">
-              <img className="img-fluid" src="sponsors/unil.png" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="visual-footer hidden-lg-up">
-        <div className="container-fluid">
-          
-        </div>
-      </div>
+      
+      <Switch>
+        <Route exact path="/" render={() => <DefaultLayout><Home /></DefaultLayout>} />
+        <Route exact path="/tv" render={() => <TvLayout><Tv /></TvLayout>} />
+      </Switch>
 
 
     </div>
---
+++ src/Tv.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import News from './News.js'
+import Instagram from './Instagram.js'
+import FacebookNew from './FacebookNew.js'
+import Facebook from './Facebook.js'
+import GoogleMap from './GoogleMap.js'
+import Stats from './Stats.js'
+import SectionTitleDesktop from './SectionTitleDesktop.js'
+import SectionTitle from './SectionTitle.js'
+import FacebookProvider, { EmbeddedPost } from 'react-facebook';
+import Scrollspy from 'react-scrollspy'
+import './tv.css'
+
+class Tv extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-5">
+            <div className="youtube-container">
+              <News youtube/>
+            </div>
+            <div className="map-container">
+              <GoogleMap />
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="insta-container">
+              <Instagram postsWidth="320"/>
+            </div>
+            <div className="stats-container">
+              <Stats />
+            </div>
+          </div>
+          <div className="col-4">
+            <FacebookNew postsWidth="320"/>
+          </div>
+        </div>
+      </div>
+  	)
+  }
+}
+
+export default Tv;

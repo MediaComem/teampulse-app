47967c0dd768d5b27bdf5569ce1ed2eca249d6cc
Oreonax <esok@mailoo.org>
2017-05-23 13:56:48 +0200

    Add localTime and missing stats values

--- src/Home.js
+++ src/Home.js
@@ -65,23 +65,24 @@ class Home extends Component {
           <div className="container">
             <div className="stats-desktop-container">
               <div className="row">
-                <div className="stat col-2"><SuperStat type="rate" displayFlex={false} descr="cadence moyenne" unit="rmp"/></div>
                 <div className="stat col-2"><SuperStat type="speed" displayFlex={false} descr="Vitesse moyenne" unit="km/h"/></div>
-                <div className="stat col-2"><SuperStat type="power" displayFlex={false} descr="Puissance moyenne" unit=""/></div>
-                <div className="stat col-2"><SuperStat type="rate" displayFlex={false} descr="Puissance moyenne" unit=""/></div>
-                <div className="stat col-2"><SuperStat type="rate" displayFlex={false} descr="Puissance moyenne" unit=""/></div>
-                <div className="stat last col-2"><SuperStat type="rate" displayFlex={false} descr="Puissance moyenne" unit=""/></div>
+                <div className="stat col-2"><SuperStat type="rate" displayFlex={false} descr="Cadence moyenne" unit="rmp"/></div>
+                <div className="stat col-2"><SuperStat type="time" displayFlex={false} descr="Heure locale" unit="hh:mm"/></div>
+                <div className="stat col-2"><SuperStat type="alt" displayFlex={false} descr="Altitude" unit="m"/></div>
+                <div className="stat col-2"><SuperStat type="temp" displayFlex={false} descr="Température extérieure" unit="°C"/></div>
+                <div className="stat last col-2"><SuperStat type="power" displayFlex={false} descr="Puissance moyenne" unit="watts"/></div>
               </div>
             </div>
             <GoogleMap />
             <div id="statsSlider" className="stats-mobile-container">
               <Slider {...settings}>
-                <div><SuperStat type="rate" displayFlex={true} descr="cadence moyenne" unit="rmp"/></div>
-                <div><SuperStat type="speed" displayFlex={true} descr="Vitesse moyenne" unit="km/h"/></div>
+                <div><SuperStat type="speed" displayFlex={false} descr="Vitesse moyenne" unit="km/h"/></div>
+                <div><SuperStat type="rate" displayFlex={false} descr="Cadence moyenne" unit="rmp"/></div>
                 <div><SuperStat type="power" displayFlex={true} descr="Puissance moyenne" unit=""/></div>
-                <div><SuperStat displayFlex={true} descr="Puissance moyenne" unit=""/></div>
-                <div><SuperStat displayFlex={true} descr="Puissance moyenne" unit=""/></div>
-                <div><SuperStat displayFlex={true} descr="Puissance moyenne" unit=""/></div>
+                <div><SuperStat type="time" displayFlex={false} descr="Heure locale" unit="hh:mm"/></div>
+                <div><SuperStat type="alt" displayFlex={false} descr="Altitude" unit="m"/></div>
+                <div><SuperStat type="temp" displayFlex={false} descr="Température extérieure" unit="°C"/></div>
+                <div><SuperStat type="power" displayFlex={false} descr="Puissance moyenne" unit="watts"/></div>
               </Slider>
             </div>
             <SectionTitleDesktop left={-20} bottom={-40} padding="10px 20px" bgColor="#A6C222" txtColor="#fff">La carte</SectionTitleDesktop>
--- src/Numbers.js
+++ src/Numbers.js
@@ -5,21 +5,21 @@ import { Event } from 'react-socket-io';
 
 const displayFlex = {
   display: "flex",
-  marginLeft:0,
-  marginRight:0,
-  alignItems:"center"
+  marginLeft: 0,
+  marginRight: 0,
+  alignItems: "center"
 }
 
 const cssStatNum = {
-  width:"50%",
-  textAlign:"right",
-  paddingRight:"3px"
+  width: "50%",
+  textAlign: "right",
+  paddingRight: "3px"
 }
 
 const cssStatDescr = {
-  width:"30%",
-  textAlign:"left",
-  paddingLeft:"3px"
+  width: "30%",
+  textAlign: "left",
+  paddingLeft: "3px"
 }
 
 const StatContainer = styled.div`
@@ -35,44 +35,77 @@ const StatNum = styled.div`
 
 const StatDescr = styled.div`
   color:#A6C222;
+  text-transform: uppercase;
   ${(props) => props.displayFlex === true ? cssStatDescr : null}
+`;
+
+const StatUnit = styled.span`
+  text-transform: none;
 `;
 
 export class SuperStat extends Component {
 
   constructor(props, context) {
     super(props, context);
-    this.state = {powerValue: 0};
+    this.state = { powerValue: 0, localTime: new Date(), rawOffset: 0, dstOffset: 0 };
     this.onMessage = this.onMessage.bind(this);
   }
 
   componentDidMount() {
-    fetch(constantes.serverUrl+'/teampulse/data')
+    fetch(constantes.serverUrl + '/teampulse/data')
       .then(response => response.json())
       .then((body) => {
-        this.setState({powerValue:parseFloat(body.avgPower).toFixed(1),rateValue:parseFloat(body.avgCadence).toFixed(1),speedValue:parseFloat(body.avgSpeed).toFixed(1)});
+        this.setState({ powerValue: parseFloat(body.avgPower).toFixed(1), rateValue: parseFloat(body.avgCadence).toFixed(1), speedValue: parseFloat(body.avgSpeed).toFixed(1), tempValue: parseFloat(body.temperature).toFixed(1), altValue: parseInt(body.altitude), rawOffset: parseInt(body.rawOffset), dstOffset: parseInt(body.dstOffset) });
       });
+    this.timerID = setInterval(
+      () => this.updateTime(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
 
   onMessage(message) {
-    this.setState({powerValue:parseFloat(message.avgPower).toFixed(1),rateValue:parseFloat(message.avgCadence).toFixed(1),speedValue:parseFloat(message.avgSpeed).toFixed(1)});
+    this.setState({ powerValue: parseFloat(message.avgPower).toFixed(1), rateValue: parseFloat(message.avgCadence).toFixed(1), speedValue: parseFloat(message.avgSpeed).toFixed(1), tempValue: parseFloat(message.temperature).toFixed(1), altValue: parseInt(message.altitude), rawOffset: parseInt(message.rawOffset), dstOffset: parseInt(message.dstOffset) });
+  }
+
+  calcLocalTime(offset) {
+    var utc = new Date().getTime();
+    return new Date(utc +  (1000 * offset));
+  }
+
+  updateTime() {
+    this.setState({
+      localTime: this.calcLocalTime(this.state.rawOffset + this.state.dstOffset)
+    });
   }
 
   render() {
     var statValue = <StatNum>N/A</StatNum>
-    if(this.props.type === 'speed'){
+    if (this.props.type === 'speed') {
       statValue = <StatNum displayFlex={this.props.displayFlex}>{this.state.speedValue}</StatNum>
     }
-    if(this.props.type === 'rate'){
+    if (this.props.type === 'rate') {
       statValue = <StatNum displayFlex={this.props.displayFlex}>{this.state.rateValue}</StatNum>
     }
-    if(this.props.type === 'power'){
+    if (this.props.type === 'power') {
       statValue = <StatNum displayFlex={this.props.displayFlex}>{this.state.powerValue}</StatNum>
+    }
+    if (this.props.type === 'temp') {
+      statValue = <StatNum displayFlex={this.props.displayFlex}>{this.state.tempValue}</StatNum>
+    }
+    if (this.props.type === 'alt') {
+      statValue = <StatNum displayFlex={this.props.displayFlex}>{this.state.altValue}</StatNum>
+    }
+    if (this.props.type === 'time') {
+      statValue = <StatNum displayFlex={this.props.displayFlex}>{this.state.localTime.getUTCHours()}:{this.state.localTime.getUTCMinutes()}</StatNum>
     }
     return (
       <StatContainer displayFlex={this.props.displayFlex} className={this.props.className}>
         {statValue}
-        <StatDescr displayFlex={this.props.displayFlex}>{this.props.descr}&nbsp;{this.props.unit}</StatDescr>
+        <StatDescr displayFlex={this.props.displayFlex}>{this.props.descr}&nbsp;<StatUnit>({this.props.unit})</StatUnit></StatDescr>
         <Event event='teampulse' handler={this.onMessage} />
       </StatContainer>
     )

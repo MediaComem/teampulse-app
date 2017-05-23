5a2ad16b95343ae279f37acb1df2e75071d16391
Raphael Baumann <wayoffproduction@gmail.com>
2017-05-23 11:13:12 +0200

    fix drifting line

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -11,14 +11,17 @@ const MapContainer = styled.div`
 
 const CycloMarker = styled.div`
   position: absolute;
-  width: ${props => (props.radius)*2}px;
-  height: ${props => (props.radius)*2}px;
-  left: ${props => (props.radius)}px;
-  top: ${props => (props.radius)}px;
+  width: ${props => (props.radius)}px;
+  height: ${props => (props.radius)}px;
+  left: ${props => -(props.radius)/2}px;
+  top: ${props => -(props.radius)/2}px;
 
-  border: 5px solid #f44336;
-  borderRadius: ${props => (props.radius)*2}px;
+  border: 2px solid #f44336;
+  borderRadius: ${props => (props.radius)}px;
   backgroundColor: white;
+  textAlign: center;
+  color: #3f51b5;
+  fontSize: 16;
 `;
 
 class GoogleMap extends Component {

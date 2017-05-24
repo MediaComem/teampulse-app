2edfe9eefdaff5208029a1d5d171b643e1732cc8
Oreonax <esok@mailoo.org>
2017-05-24 11:22:23 +0200

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-99848921-1');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 ReactDOM.render(
-  <App />,
+  <App onUpdate={logPageView} />,
   document.getElementById('root')
 );

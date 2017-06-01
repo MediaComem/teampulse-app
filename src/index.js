e626240392ac71067635b95ba5de08879f6cfac4
Oreonax <esok@mailoo.org>
2017-06-01 09:38:55 +0200

    Replace google analytics component with raw script in index.html

--- src/index.js
+++ src/index.js
@@ -4,15 +4,7 @@ import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-99848921-1');
-
-function logPageView() {
-  ReactGA.set({ page: window.location.pathname + window.location.search });
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
-
 ReactDOM.render(
-  <App onUpdate={logPageView} />,
+  <App/>,
   document.getElementById('root')
 );

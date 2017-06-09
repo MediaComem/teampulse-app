e89094809205b2d14890b893abcc9d3fab87e5ac
Raphael Baumann <wayoffproduction@gmail.com>
2017-06-09 08:58:14 +0200

    fix youtube

--- src/Youtube.js
+++ src/Youtube.js
@@ -5,7 +5,6 @@ import constantes from './constantes.js';
 class Youtube extends Component {
 
 	constructor(props, context) {
-		console.log(props)
     super(props, context);
     this.state = {
       video: ""
@@ -15,24 +14,31 @@ class Youtube extends Component {
 	componentDidMount() {
     fetch(constantes.serverUrl+'/favori/data')
       .then(response => response.json())
-      .then((data) => { this.setState({video:data.data.video.id}) });
+      .then((data) => {
+				this.setState({
+					video: data.data.video.id,
+					list: data.data.video.list
+				})
+			});
   }
 
   componentWillReceiveProps(newProps) {
   if (this.state.video !== newProps.data.video.id) {
-    	this.setState({video: newProps.data.video.id});
-    	this.setState({list: newProps.data.video.list});
+    	this.setState({
+				video: newProps.data.video.id,
+				list: newProps.data.video.list
+			});
   	}
 	}
 
   render() {
 	  let link = null;
 		if(this.props.loop && this.props.playlist) {
-			link = <iframe src={"https://www.youtube.com/embed/videoseries?list="+this.state.list+"&autoplay=1&loop=1&cc_load_policy=1rel=0&amp;controls=0&amp;showinfo=0"} frameBorder="0" allowFullScreen></iframe>	
+			link = <iframe src={"https://www.youtube.com/embed/videoseries?list="+this.state.list+"&autoplay=1&loop=1&cc_load_policy=1rel=0&amp;controls=0&amp;showinfo=0"} frameBorder="0" allowFullScreen></iframe>
 		} else if(this.props.playlist) {
-			link = <iframe src={"https://www.youtube.com/embed/videoseries?list="+this.state.list} frameBorder="0" allowFullScreen></iframe>	
+			link = <iframe src={"https://www.youtube.com/embed/videoseries?list="+this.state.list} frameBorder="0" allowFullScreen></iframe>
 		} else if(this.props.loop) {
-	    link = <iframe src={"https://www.youtube.com/embed/"+this.state.video + "?autoplay=1&loop=1&cc_load_policy=1rel=0&amp;controls=0&amp;showinfo=0&playlist="+this.state.video} frameBorder="0" allowFullScreen></iframe>	
+	    link = <iframe src={"https://www.youtube.com/embed/"+this.state.video + "?autoplay=1&loop=1&cc_load_policy=1rel=0&amp;controls=0&amp;showinfo=0&playlist="+this.state.video} frameBorder="0" allowFullScreen></iframe>
 		} else {
   		link = <iframe src={"https://www.youtube.com/embed/"+this.state.video} frameBorder="0" allowFullScreen></iframe>
 		}

09e9887f547d92bfadcfbb8b80ed2349cbeadb5a
Oreonax <esok@mailoo.org>
2017-06-15 10:05:33 +0200

    Prism amazing loading style

--- src/PageLoader.js
+++ src/PageLoader.js
@@ -17,17 +17,22 @@ const LoadingContainer = styled.div`
   align-items: center;
 `;
 
-const Image = styled.img`
-  width:200px;
-  height:200px;
-`;
-
 class PageLoader extends Component {
 
   render() {
     return (
       <LoadingContainer>
-        <Image className="loading" src="loading.svg" alt="" />
+        <div className="vertical-centered-box">
+          <div className="content">
+            <div className="loader-circle"></div>
+            <div className="loader-line-mask">
+              <div className="loader-line"></div>
+            </div>
+            <svg width="50" viewBox="0 0 91.2 91.2" id="prism-logo">
+              <path d="M16 66V33c0-1 .4-1.8 1-2.4.7-.7 1.5-1 2.4-1h46.8c3.6 0 6.7 1.3 9.3 3.8 2.6 2.6 3.8 5.7 3.8 9.3 0 3.6-1.3 6.7-3.8 9.3-2.6 2.5-5.7 3.8-9.3 3.8H23V66c0 1-.3 1.8-1 2.4-.7.7-1.5 1-2.4 1s-1.8-.3-2.4-1c-.8-.7-1-1.5-1-2.4zm50.3-29.8H23v13h43.3c1.8 0 3.3-.7 4.6-2 1.2-1.2 1.8-2.8 1.8-4.6s-.6-3.3-2-4.6-2.7-1.8-4.5-1.8z" fill="#A6C222" />
+            </svg>
+          </div>
+        </div>
       </LoadingContainer >
     )
   }
